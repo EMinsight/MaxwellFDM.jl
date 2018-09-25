b4940a19c42041c14c4db08f7877b4d460ebfb26
Wonseok Shin <wsshin97@gmail.com>
2018-09-25 11:39:00 -0400

    Uncomment unit tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,18 +7,18 @@ Base.isapprox(a::Tuple, b::Tuple; kws...) = all(p -> isapprox(p...; kws...), zip
 
 # @testset "MaxwellFDM" begin
 
-# include("enumtype.jl")
-# include("base.jl")
-# include("phys.jl")
-# include("grid.jl")
-# include("object.jl")
-# include("gridgen.jl")
-# include("material.jl")
-# include("smoothing.jl")
-# include("differential.jl")
-# include("mean.jl")
-# include("param.jl")
+include("enumtype.jl")
+include("base.jl")
+include("phys.jl")
+include("grid.jl")
+include("object.jl")
+include("gridgen.jl")
+include("material.jl")
+include("smoothing.jl")
+include("differential.jl")
+include("mean.jl")
+include("param.jl")
 include("pml.jl")
-# include("source.jl")
+include("source.jl")
 
 # end  # @testset "MaxwellFDM"
