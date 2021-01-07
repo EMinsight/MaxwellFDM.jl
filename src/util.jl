ac810f048f78df2cca6bbb9b21c47958468b5679
Wonseok Shin <wsshin97@gmail.com>
2021-01-06 23:21:23 -0500

    Move isfield_ortho_shape() from smoothing.jl to util.jl in order to use it in source/*.jl

--- src/util.jl
+++ src/util.jl
@@ -93,61 +93,58 @@ isuniform(mv::MArray{Tuple{2,2,2},<:Any,3,8}) = @inbounds (mv[1]==mv[2]==mv[3]==
 isuniform(mv::MArray{Tuple{2,2},<:Any,2,4}) = @inbounds (mv[1]==mv[2]==mv[3]==mv[4])
 isuniform(mv::MArray{Tuple{2},<:Any,1,2}) = @inbounds (mv[1]==mv[2])
 
-
-# getindex(t::Tuple3{T}, ind::Tuple3{Int}) where {T} = (t[ind[1]], t[ind[2]], t[ind[3]])
-# getindex(t::Tuple32{T}, i::Int, j::Int, k::Int) where {T} = (t[1][i], t[2][j], t[3][k])  # i, j, k = 1 or 2
-# getindex(t::Tuple23{T}, i::Int, j::Int, k::Int) where {T} = (t[i][1], t[j][2], t[k][3])  # i, j, k = 1 or 2
-# getindex(t::Tuple3{AbsVec{T}}, i::Int, j::Int, k::Int) where {T} = (t[1][i], t[2][j], t[3][k])
-# getindex(t::Tuple3{AbsVec{T}}, i::Tuple2{Int}, j::Tuple2{Int}, k::Tuple2{Int}) where {T} =
-#     ((t[1][i[1]], t[1][i[2]]), (t[2][j[1]], t[2][j[2]]), (t[3][k[1]], t[3][k[2]]))
-
-# rand3() = (rand(), rand(), rand())
-# randn3() = (randn(), randn(), randn())
+# Determine if the field subspace is orthogonal to the shape subspace; used in smoothing.jl
+# and source/*.jl.
 #
-# dot(t1::Tuple3{Real}, t2::Tuple3{Real}) = t1[1]*t2[1] + t1[2]*t2[2] + t1[3]*t2[3]
+# The shape and field subspaces are the subspaces of the 3D space where the shapes and
+# fields lie.  In standard 3D problems, both subspaces are 3D.  However there are other
+# cases as well: for example, in 2D TM problems, the shapes are on the xy-plane (2D space),
+# but the E-fields are along the z-direction (1D space).
 #
-# function norm2(x)
-#     if isa(x, Number)
-#         return abs(x)
-#     else
-#         sx = start(x)
+# For the shape and field subspace dimensions K and Kf, orthogonal subspaces can occur only
+# when K + Kf ≤ 3, because if two subspaces are orthogonal to each other and K + Kf > 3, we
+# can choose K + Kf linearly independent vectors in the direct sum of the two subspaces,
+# which is impossible as the direct sum should still be a subspace of the 3D space.
 #
-#         result = 0.0
-#         (xi, sx) = next(x, sx)
-#         result += norm2(xi)^2
-#         while !done(x, sx)
-#             (xi, sx) = next(x, sx)
-#             result += norm2(xi)^2
-#         end
+# Considering the above, there are only a few combinations of K and Kf that allow orthogonal
+# subspaces: (K,Kf) = (2,1), (1,1), (1,2).
+# - (K,Kf) = (2,1).  This happens in 2D TE or TM problems.  The shape subspace is the 2D xy-
+# plane, but the magnetic (electric) field subspace in TE (TM) problems is the 1D z-axis.
+# - (K,Kf) = (1,1).  This happens in the 1D TEM problems with isotropic materials.  The
+# shape subspace is the 1D z-axis, but the E- and H-field spaces are the 1D x- and y-axes.
+# - (K,Kf) = (1,2).  This happens in the 1D TEM problem with anisotropic materials.
 #
-#         return sqrt(result)
-#     end
-# end
+# Note that we can always solve problems as if they are 3D problems.  So, the consideration
+# of the cases with K, Kf ≠ 3 occurs only when we can build special equations with reduced
+# number of degrees of freedom, like in the TE, TM, TEM equations.  In such cases, we find
+# that the two subspaces are always orthogonal if K ≠ Kf.  In fact, smooth_param!() is
+# written such that the Kottke's subpixel smoothing algorithm that decomposes the field into
+# the components tangential and normal to the shape surface is applied only when the shape
+# and field subspaces coincide.  (This makes sense, because the inner product between the
+# field and the direction normal that needs to be performed to decompose the field into such
+# tangential and normal components is defined only when the field and the direction normal
+# are in the same vector space.)  Therefore, if we want to apply Kottke's subpixel smoothing
+# algorithm that depends on the surface normal direction, we have to construct the problem
+# such that K = Kf (but the converse is not true: K = Kf does not imply the use of Kottke's
+# subpixel smoothing algorithm that depends on the surface normal direction; in other words,
+# the case with K = Kf can still use the subpixel smoothing algorithm that assumes the
+# orthogonality between the shape and field subspace, such that the field is always
+# tangential to the shape surface.)
 #
-# function norm2diff(x, y)
-#     if isa(x, Number)
-#         if !isa(y, Number)
-#             return Inf
-#         else
-#             return abs(x-y)
-#         end
-#     elseif (isa(x,AbsArr) && isa(y,Tuple)) || (isa(x,Tuple) && isa(y,AbsArr))
-#         return Inf
-#     else
-#         sx, sy = start(x), start(y)
+# The contraposition of the above statement is that if K ≠ Kf, then Kottke's subpixel
+# smoothing algorithm that does NOT depend on the direction normal is applied.  This
+# subpixel smoothing algorithm assumes that the field subspace is orthogonal to the shape
+# subspace, such that the field has only the tangential component to the surface.
+# Therefore, if we pass K ≠ Kf to smooth_param!(), we should make sure that the field
+# subspace is orthogonal to the shape subspace.  Note that this does not exclude the
+# possibility of K ≠ Kf while the two subspaces are nonorthogonal; it just means that we
+# must formulate the problem differently in such cases by, e.g., decomposing the equations,
+# in order to use smooth_param!().
 #
-#         result = 0.0
-#         (xi, sx), (yi, sy) = next(x, sx), next(y, sy)
-#         result += norm2diff(xi, yi)^2
-#         while !done(x, sx) && !done(y, sy)
-#             (xi, sx), (yi, sy) = next(x, sx), next(y, sy)
-#             result += norm2diff(xi, yi)^2
-#         end
-#
-#         if !done(x, sx) || !done(y, sy)
-#             return Inf
-#         else
-#             return sqrt(result)
-#         end
-#     end
-# end
+# As noted earlier, K = Kf can stil include cases where the shape and field subspaces are
+# orthogonal.  Because K + Kf = 2K should be ≤ 3 in+ such cases, we conclude that K = Kf = 1
+# is the only case where the two subspaces could be orthogonal while K = Kf.  In fact, when
+# K = Kf, we will assume that the two subspaces are orthogonal, because the problems with 1D
+# slabs and the field along the slab thickness direction are not interesting from the EM
+# wave propagation perspectives.
+isfield_ortho_shape(Kf, K) = Kf≠K || Kf==1
